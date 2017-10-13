9e7643afa284c82fc1abbea02fdbb0085ed4adfc
TchernyavskyDaniil <tchernyavskydaniil@MacBook-Pro-TchernyavskyDaniil.local>
2017-10-13 16:39:13 +0300

    sprite new version for background svg

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,53 @@ var server = require("browser-sync").create();
 var svgSprite = require("gulp-svg-sprite");
 
 var config = {
- shape: {
- spacing: { // Add padding
- padding: 20
- }
- },
- mode: {
- css: { // Activate the «css» mode
- render: {
- css: true // Activate CSS output (with default options)
- }
- },
- symbol: true
- }
+  shape: {
+    dimension		: {			// Set maximum dimensions
+
+    },
+    spacing: { // Add padding
+      padding			: 20,						// Padding around all shapes
+      box				: 'content'
+    }
+  },
+  mode: {
+    view: { // Activate the «css» mode
+      bust: false,
+      sprite: "../sprite/sprite.svg"
+    },
+  },
 };
 
+var configS = {
+  shape: {
+    spacing: {
+      padding: 0
+    }
+  },
+  mode: {
+    symbol: {
+      sprite: "../../img/sprite/sprite-symbols.svg"
+    }
+  },
+};
+
+// var configdest = {
+//   sprite: {
+//     "kek.svg"
+//   },
+// };
 
 gulp.task("sprite", function () {
   return gulp.src("img/*.svg")
   .pipe(svgSprite(config))
-  .pipe(gulp.dest("img"));
+    .pipe(gulp.dest("img"));
 });
+
+gulp.task("spriteSymbol", function() {
+  return gulp.src("img/*.svg")
+    .pipe(svgSprite(configS))
+    .pipe(gulp.dest("img"));
+})
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
